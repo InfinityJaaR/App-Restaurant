a62a1523612df57f14ab08441f16165b003d5b64
InfinityJaaR <javieralfaroromero@gmail.com>
2024-11-27 10:58:58 -0600

    Agregando pagina inicio dinamica

--- app/views.py
+++ app/views.py
@@ -5,11 +5,28 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from django.contrib import messages
+from django.contrib.auth import logout
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    user = request.user
+    is_admin = user.groups.filter(name='Administrador').exists()
+    is_client = user.groups.filter(name='Cliente').exists()
+    is_repartidor = user.groups.filter(name='Repartidor').exists()
+    is_despachador = user.groups.filter(name='Encargado de Despachos').exists()
+    is_menu = user.groups.filter(name='Encargado de Menu').exists()
+    is_registros = user.groups.filter(name='Registro de Pedidos').exists()
+    context = {
+        'user': user,
+        'is_admin': is_admin,
+        'is_client': is_client,
+        'is_repartidor': is_repartidor,
+        'is_despachador': is_despachador,
+        'is_menu': is_menu,
+        'is_registros': is_registros
+    }
+    return render(request, 'home.html', context)
 
 @login_required
 def logout_view(request):
@@ -39,3 +56,13 @@ def registro(request):
         else:
             messages.error(request, 'Las contraseñas no coinciden')
     return render(request, 'login')
+
+@login_required
+def nuevoUsuario (request):
+    if request.method == 'POST':
+        nombre = request.POST['first_name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        password = request.POST['password1']
+        password2 = request.POST['password2']
+        groups = request.POST['groups']
