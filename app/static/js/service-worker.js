0edeb6d748cd146a5382668d486adb5655e41a2d
Emerson Arevalo <AO22002@ues.edu.sv>
2024-12-04 17:45:54 -0600

    Agregando la funsionalidad de PWA

---
+++ app/static/js/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "pedidos-cache-v1";
+const urlsToCache = [
+    "/accounts/login/",
+    "/home/",
+    "/static/css/login.css",
+    "/static/css/nav.css",
+    "/static/img/faviconW-SF.png",
+    "/static/favicon.ico",
+];
+
+// Instalar el Service Worker y cachear recursos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch((error) => {
+        console.error("Error al agregar recursos al caché:", error);
+      });
+    })
+  );
+});
+
+// Interceptar solicitudes para servir desde caché
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activar el Service Worker y limpiar cachés antiguas
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
