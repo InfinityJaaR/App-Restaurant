10c1f1eb3130a04a0390b56beda04a59430955c0
Emerson Arevalo <AO22002@ues.edu.sv>
2024-12-04 17:59:27 -0600

    Revert "Merge pull request #14 from InfinityJaaR/emerson"
    This reverts commit 0d0d2915d7d48607a1cec32ff5c3b3213468abf1, reversing
    changes made to 28f5f105df7ecae198bf3343a8649521e174fd33.

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -81,11 +81,6 @@ WSGI_APPLICATION = 'restaurante.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    #  'default': dj_database_url.config(
-    #     # Replace this value with your local database's connection string.
-    #     default='postgresql://postgres:postgres@localhost:5432/mysite',
-    #     conn_max_age=600
-    # )
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
@@ -129,16 +124,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR /"app" / 'static']
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-# if not DEBUG:
-#     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     # and renames the files with unique names for each version to support long-term caching
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
