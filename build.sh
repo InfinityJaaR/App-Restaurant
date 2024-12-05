0472dbcd0130d75de2b03625f906ce61aa125fee
InfinityJaaR <javieralfaroromero@gmail.com>
2024-12-04 18:21:08 -0600

    Para deploy

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
+
+# Crear superusuario
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', '123456789')"
+
+# Create a new user
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('administrador', password='123456789')"
