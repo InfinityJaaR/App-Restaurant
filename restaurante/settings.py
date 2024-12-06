f7c2132d9b540edfdb92908a681df709933e5a59
InfinityJaaR <javieralfaroromero@gmail.com>
2024-12-05 21:54:57 -0600

    Para deploy 15

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -145,5 +145,5 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/'
-MEDIA_ROOT =os.path.join(BASE_DIR)
+MEDIA_URL = 'platillos/'
+MEDIA_ROOT =os.path.join(BASE_DIR, 'platillos')
