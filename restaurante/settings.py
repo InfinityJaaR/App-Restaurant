b25ca32daed00326844abdee023b8ba02e9dcdf6
InfinityJaaR <javieralfaroromero@gmail.com>
2024-12-06 18:21:39 -0600

    Para deploy 25

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -145,5 +145,5 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/'
-MEDIA_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
